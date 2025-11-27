c2b0ba790fe85bbc1498a5f66652308a15cdebba
Davinsry <davinipad53@gmail.com>
2025-11-27 13:29:53 +0700

    Menambahkan object destinasi home

---
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan9.view.route
+
+import com.example.pertemuan9.R
+
+object DestinasiHome : Destinasinavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+
+}
---
+++ app/src/main/java/com/example/pertemuan9/view/route/Destinasinavigasi.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan9.view.route
+
+interface Destinasinavigasi {
+
+    val route: String
+
+    val titleRes: Int
+}
