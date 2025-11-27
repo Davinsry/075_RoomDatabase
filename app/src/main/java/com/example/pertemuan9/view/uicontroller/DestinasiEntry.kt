0ff680105356cdddbd531a7a8fda4ab8d5adccad
Davinsry <davinipad53@gmail.com>
2025-11-27 13:43:05 +0700

    Menambahkan data pada EntryViewModel.kt

---
+++ app/src/main/java/com/example/pertemuan9/view/uicontroller/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan9.view.uicontroller
+
+import com.example.pertemuan9.R
+import com.example.pertemuan9.view.route.Destinasinavigasi
+
+class DestinasiEntry: Destinasinavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
