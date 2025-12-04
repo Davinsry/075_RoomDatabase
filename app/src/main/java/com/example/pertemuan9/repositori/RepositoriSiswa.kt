a07640eeed218e4a24ba8f0ba131299c7d892067
Davinsry <davinipad53@gmail.com>
2025-12-04 11:37:13 +0700

    menambah override pada RepositoriSiswa.kt

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -7,6 +7,11 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa{
     fun getAllSiswaStream(): Flow<List<Siswa>>
 
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+
+    suspend fun deleteSiswa(siswa: Siswa)
+
+
     suspend fun insertSiswa(siswa: Siswa)
 
 }
@@ -17,5 +22,12 @@ class OfflineRepositoriSiswa(
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
 
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+
+
+
 
 }
